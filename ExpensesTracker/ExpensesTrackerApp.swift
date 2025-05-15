88c9e76f795000ef5cd4a04765f5be745d8a3389
Salma Hassan <salma.kamaleldin@vodafone.com>
2025-05-15 03:37:12 +0300

    Initial Commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Salma Kamal Eldin, Vodafone on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
